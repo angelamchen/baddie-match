09bafe20e11186014cc342f776f02a4381de9af8
Angela Chen <angelachen085@gmail.com>
2022-02-12 14:30:27 -0500

    deploying to the webs

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from flask_cors import CORS #comment this on deployment
 
-#app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
 
 CORS(app)
 @app.route('/')
 def index():
-    return "Hello world"
-    #return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 @app.route("/hello")
 def hello_world():
     return "We track badminton tings"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
